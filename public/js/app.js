c53006c4de02d982c62812257da8a64ae5b32e96
pisha <pisha@hotmail.fr>
2020-09-21 14:43:49 +0200

    ajout des animations des boutons

--- public/js/app.js
+++ public/js/app.js
@@ -33,12 +33,17 @@ var tween = TweenMax.to("#raptor", 1, {left:200, ease: Power4.easeIn, onComplete
 
 var tween2 = TweenMax.to("#trex", 1, {left:-200, ease: Power4.easeIn, onComplete : TrexSound})
 
-var tweenLi = TweenMax.staggerFromTo('.raptorLi', 0.75,  {opacity: 0, y: -100, ease:Power4.easeIn}, 
-                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1) 
+var tweenLi = TweenMax.staggerFromTo('.raptorLi', 0.75, {opacity: 0, y: -100, ease:Power4.easeIn}, 
+                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1); 
 
 var tweenLi2 = TweenMax.staggerFromTo('.trexLi', 0.75,  {opacity: 0, y: -100, ease:Power4.easeIn}, 
-                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1) 
+                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1);
 
+var tweenBtn = TweenMax.staggerFromTo('.btn1', 0.75,    {opacity:0, x:-100, ease:Power4.easeIn},
+                                                        {opacity:1, x:0, ease:Power4.easeIn},0.1);
+
+var tweenBtn2 = TweenMax.staggerFromTo('.btn2', 0.75,   {opacity:0, x:100, ease:Power4.easeIn},
+                                                        {opacity:1, x:0, ease:Power4.easeIn},0.1);
 
 /* Scene ScrollMagic */
 
@@ -88,6 +93,32 @@ var scene4 = new ScrollMagic.Scene({triggerElement: "#animate2", offset: 100})
                                   .addIndicators({name: "li2", colorTrigger: "blue", indent: 400})
                                   /* Liaison à Greensock */
                                   .setTween(tweenLi2)
+                                  /* Reverse false pour que l'anim ne se rejoue pas */
+                                  .reverse(false)
+                                  /* Ajout de la scène au controlleur */
+                                  .addTo(controller)
+                                  
+
+var scene5 = new ScrollMagic.Scene({triggerElement: "#animate1", offset: 50})
+                                  /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
+                                  .triggerHook(0.5)
+                                  /* Ajout du curseur repère pour trigger */
+                                  .addIndicators({name: "btn1", colorTrigger: "blue", indent: 400})
+                                  /* Liaison à Greensock */
+                                  .setTween(tweenBtn)
+                                  /* Reverse false pour que l'anim ne se rejoue pas */
+                                  .reverse(false)
+                                  /* Ajout de la scène au controlleur */
+                                  .addTo(controller)
+                                  
+
+var scene6 = new ScrollMagic.Scene({triggerElement: "#animate2", offset: 100})
+                                  /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
+                                  .triggerHook(0.8)
+                                  /* Ajout du curseur repère pour trigger */
+                                  .addIndicators({name: "btn2", colorTrigger: "blue", indent: 400})
+                                  /* Liaison à Greensock */
+                                  .setTween(tweenBtn2)
                                   /* Reverse false pour que l'anim ne se rejoue pas */
                                   .reverse(false)
                                   /* Ajout de la scène au controlleur */
