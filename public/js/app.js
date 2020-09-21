c803be8290bf9631e4700f36dff57abb87dea122
pisha <pisha@hotmail.fr>
2020-09-21 13:28:40 +0200

    animation des textes

--- public/js/app.js
+++ public/js/app.js
@@ -33,15 +33,20 @@ var tween = TweenMax.to("#raptor", 1, {left:200, ease: Power4.easeIn, onComplete
 
 var tween2 = TweenMax.to("#trex", 1, {left:-200, ease: Power4.easeIn, onComplete : TrexSound})
 
+var tweenLi = TweenMax.staggerFromTo('.raptorLi', 0.75,  {opacity: 0, y: -100, ease:Power4.easeIn}, 
+                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1) 
+
+var tweenLi2 = TweenMax.staggerFromTo('.trexLi', 0.75,  {opacity: 0, y: -100, ease:Power4.easeIn}, 
+                                                        {opacity: 1, y: 0, ease:Power4.easeIn}, 1) 
 
 
 /* Scene ScrollMagic */
 
-var scene = new ScrollMagic.Scene({triggerElement: "#animate1", offset: 500,})
+var scene = new ScrollMagic.Scene({triggerElement: "#animate1", offset: 50})
                                   /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
-                                  .triggerHook(0.9)
+                                  .triggerHook(0.5)
                                   /* Ajout du curseur repère pour trigger */
-                                  .addIndicators({name: "vélociraptor", colorTrigger: "red", indent: 400})
+                                  .addIndicators({name: "raptor", colorTrigger: "blue",  indent: 400})
                                   /* Liaison à Greensock */
                                   .setTween(tween)
                                   /* Reverse false pour que l'anim ne se rejoue pas */
@@ -57,6 +62,32 @@ var scene2 = new ScrollMagic.Scene({triggerElement: "#animate2", offset: 100})
                                   .addIndicators({name: "trex", colorTrigger: "blue", indent: 500})
                                   /* Liaison à Greensock */
                                   .setTween(tween2)
+                                  /* Reverse false pour que l'anim ne se rejoue pas */
+                                  .reverse(false)
+                                  /* Ajout de la scène au controlleur */
+                                  .addTo(controller)
+                                  
+
+var scene3 = new ScrollMagic.Scene({triggerElement: "#animate1", offset: 50})
+                                  /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
+                                  .triggerHook(0.5)
+                                  /* Ajout du curseur repère pour trigger */
+                                  .addIndicators({name: "li1", colorTrigger: "green", indent: 500})
+                                  /* Liaison à Greensock */
+                                  .setTween(tweenLi)
+                                  /* Reverse false pour que l'anim ne se rejoue pas */
+                                  .reverse(false)
+                                  /* Ajout de la scène au controlleur */
+                                  .addTo(controller)
+                                  
+
+var scene4 = new ScrollMagic.Scene({triggerElement: "#animate2", offset: 100})
+                                  /*  quand le triggerhook va passer à l'endroit du trigger element cela va lancer l'animation */
+                                  .triggerHook(0.8)
+                                  /* Ajout du curseur repère pour trigger */
+                                  .addIndicators({name: "li2", colorTrigger: "blue", indent: 400})
+                                  /* Liaison à Greensock */
+                                  .setTween(tweenLi2)
                                   /* Reverse false pour que l'anim ne se rejoue pas */
                                   .reverse(false)
                                   /* Ajout de la scène au controlleur */
